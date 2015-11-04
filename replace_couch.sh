18cfa9bf280b3033ee0ef9a27bdb94844a58c12f
Bryan <btbonval@gmail.com>
2015-11-04 17:09:15 -0500

    debug and switched var order around

--- replace_couch.sh
+++ replace_couch.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 DEMOSDIR=./demos
-COUCHDIR=`curl ${COUCHURL}/_config/couchdb/database_dir 2> /dev/null | sed s/\"//g`
 COUCHURL="http://localhost:5984"
+COUCHDIR=`curl ${COUCHURL}/_config/couchdb/database_dir 2> /dev/null | sed s/\"//g`
+echo "Found CouchDB at ${COUCHDIR}"
 
 # fetch db snapshot
 wget http://dev.medicmobile.org/downloads/temp/medic-demos-release-20151104.tar.xz
@@ -14,6 +15,7 @@ sudo service couchdb stop
 sudo cp ${DEMOSDIR}/* ${COUCHDIR}
 # determine the couchdb unix user/group
 COUCHOWNER=`ls -ld ${COUCHDIR} | awk '{print $3 ":" $4}'`
+echo "Found CouchDB owner:group to be ${COUCHOWNER}"
 # set the correct owner for the new files
 sudo chown -R ${COUCHOWNER} ${COUCHDIR}
 # start up couchdb
