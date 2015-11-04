56684ba047e3c9531a841bdde254f376f8897295
Bryan <btbonval@gmail.com>
2015-11-04 16:51:35 -0500

    fetch built binary and load that into CouchDB on Travis

---
+++ replace_couch.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+DEMOSDIR=./demos
+COUCHDIR=`curl ${COUCHURL}/_config/couchdb/database_dir 2> /dev/null | sed s/\"//g`
+COUCHURL="http://localhost:5984"
+
+# fetch db snapshot
+wget http://dev.medicmobile.org/downloads/temp/medic-demos-release-20151104.tar.xz
+# set the expected output dir to DEMOSDIR
+tar -xJf medic-demos-release-20151104.tar.xz
+# turn off couchdb
+sudo service couchdb stop
+# copy couch databases over to the system
+sudo cp ${DEMOSDIR}/* ${COUCHDIR}
+# determine the couchdb unix user/group
+COUCHOWNER=`ls -ld ${COUCHDIR} | awk '{print $3 ":" $4}'`
+# set the correct owner for the new files
+sudo chown -R ${COUCHOWNER} ${COUCHDIR}
+# start up couchdb
+sudo service couchdb start
+# this script will exit when couch is online
+while [ ! `curl -f ${COUCHURL}` ]; do
+  echo "Waiting for system to come up..."
+  sleep 2
+done
