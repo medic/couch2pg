57c252392f2bdba181eb9cc6f46d8a4f479ebabc
Bryan <btbonval@gmail.com>
2015-11-23 03:44:47 -0500

    refactoring and changing doc count test
    preparations for #7

--- test/integration/index.js
+++ test/integration/index.js
@@ -16,6 +16,8 @@ var couch2pg = require('../../index');
 var pgcol = process.env.POSTGRESQL_COLUMN;
 var pgtab = process.env.POSTGRESQL_TABLE;
 var queryStr = scrub('SELECT %I FROM %I WHERE %I->>\'_id\' = %%L AND %I->>\'_rev\' = %%L;', pgcol, pgtab, pgcol, pgcol);
+var appQueryStr = scrub('SELECT %I->\'app_settings\'->\'schedules\'->0->\'messages\' FROM %I WHERE %I->\'app_settings\'->\'schedules\'->0 ? \'messages\';', pgcol, pgtab, pgcol);
+var countQueryStr = scrub('SELECT COUNT(%I) FROM %I;', pgcol, pgtab);
 
 // postgres connection
 var pgp;
@@ -38,6 +40,58 @@ var fetch_rows = new Promise(function (resolve, reject) {
     return resolve(parseInt(JSON.parse(body).total_rows));
   });
 });
+
+// verify postgres and couch match!
+function couch_in_postgres(done) {
+  // fetch all documents out of couch
+  httplib.get({ url: process.env.COUCHDB_URL },
+              function (err, httpResponse, buffer) {
+    if (err) {
+      return done(err);
+    }
+
+    // document download completed
+    var data = JSON.parse(buffer);
+    // setup a single connection for a series of thenable queries
+    var sco;
+    var queries = db.connect()
+      .then(function(cnxn) {
+        sco = cnxn;
+      });
+    // iterate over all documents
+    data.rows.forEach(function (row) {
+
+      // confirm each doc is in postgres
+      var uuid = row.id;
+      var rev = row.value.rev;
+      var thisQuery = scrub(queryStr, uuid, rev);
+      // skip design docs
+      if (uuid.slice(0,7) === '_design') { return; }
+      // iterative step: order queries with then
+      queries = queries.then(function () {
+        return sco.query(thisQuery);
+      }, handleError)
+        .then(function(result) {
+          if (result[0] === undefined) {
+            throw Error('cannot find result for query: ' + thisQuery);
+          }
+          var obj = result[0][pgcol];
+          return expect(obj).deep.equals(row.doc);
+        }, handleError);
+    });
+    // mark completion when the last promise has returned
+    queries.then(function () {
+        done();
+    }).catch(function (err) {
+      done(err);
+    }).finally(function () {
+      if (sco) {
+        // close the database connection
+        sco.done();
+      }
+    });
+  });
+}
 
 describe('Integration', function() {
   before(function (done) {
@@ -62,58 +116,14 @@ describe('Integration', function() {
     all(todo).then(function () {done();});
   });
 
-  it('fetches non-zero doc count', function () {
-    // make sure there are documents to be fetched.
-    return expect(n_docs).to.be.gt(0);
-  });
-
-  it('moves all non-design documents to postgres', function(done) {
-    // fetch all documents out of couch
-    httplib.get({ url: couchdb_url }, function (err, httpResponse, buffer) {
-        if (err) {
-          return done(err);
-        }
-
-        // document download completed
-        var data = JSON.parse(buffer);
-        // setup a single connection for a series of thenable queries
-        var sco;
-        var queries = db.connect()
-          .then(function(cnxn) {
-            sco = cnxn;
-          });
-        // iterate over all documents
-        data.rows.forEach(function (row) {
-
-          // confirm each doc is in postgres
-          var uuid = row.id;
-          var rev = row.value.rev;
-          var thisQuery = scrub(queryStr, uuid, rev);
-          // skip design docs
-          if (uuid.slice(0,7) === '_design') { return; }
-          // iterative step: order queries with then
-          queries = queries.then(function () {
-            return sco.query(thisQuery);
-          }, handleError)
-            .then(function(result) {
-              if (result[0] === undefined) {
-                throw Error('cannot find result for query: ' + thisQuery);
-              }
-              var obj = result[0][pgcol];
-              return expect(obj).deep.equals(row.doc);
-            }, handleError);
-        });
-        // mark completion when the last promise has returned
-        queries.then(function () {
-            done();
-        }).catch(function (err) {
-          done(err);
-        }).finally(function () {
-          if (sco) {
-            // close the database connection
-            sco.done();
-          }
-        });
+  it('imports as many rows as there are docs', function (done) {
+    // compare couchdb doc count (n_docs) to postgres rows.
+    db.one(countQueryStr).then(function (row) {
+      var docs = parseInt(row.count);
+      expect(docs).to.equal(n_docs);
+      done();
+    }, function (err) {
+      done(err);
     });
   });
 
@@ -123,8 +133,11 @@ describe('Integration', function() {
     // also good: grab a subset, like schedules and compare those.
     // there's no obvious API for that yet without grabbing the whole doc.
     // for now, just make sure the database has a doc with app_settings in it.
-    var queryAppSettings = scrub('SELECT %I->\'app_settings\'->\'schedules\'->0->\'messages\' FROM %I WHERE %I->\'app_settings\'->\'schedules\'->0 ? \'messages\';', pgcol, pgtab, pgcol);
-    return expect(db.one(queryAppSettings)).to.eventually.be.fulfilled;
+    return expect(db.one(appQueryStr)).to.eventually.be.fulfilled;
+  });
+
+  it('moves all non-design documents to postgres', function(done) {
+    couch_in_postgres(done);
   });
 
 });
