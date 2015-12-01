b79551a7d2974b92a932c56be611dc049dd64ca5
Bryan <btbonval@gmail.com>
2015-12-01 02:51:17 -0500

    turns out we were never actually checking the requested URL.
    sinon is no longer being used and so removed.

--- test/unit/couchdb.js
+++ test/unit/couchdb.js
@@ -1,5 +1,4 @@
 var http = require('http');
-var sinon = require('sinon');
 var nock = require('nock');
 
 var common = require('../common');
@@ -9,23 +8,32 @@ var cdbfuncs = require('../../cdbfuncs');
 
 var dl_data = 'cmVtIGVhcXVlIG9mZmljaWEgbGFib3J1bQp2b2x1cHRhdGVtIGVycm9yIHNlZAphY2N1c2FtdXMgbWFpb3JlcyByZXByZWhlbmRlcml0IHNpdCBoYXJ1bSBxdW9kIGVzc2Ugc2VkIGFkaXBpc2NpCm1hZ25pIGF0IGFkIHJlcnVtIHF1aWJ1c2RhbSBhbmltaSBhdXQgZXQgaXBzYW0gdm9sdXB0YXMKZXhlcmNpdGF0aW9uZW0gdXQgZWEgbmloaWwgZXVtIHF1bwpldCBtb2RpIGV0IHJlY3VzYW5kYWUgaW4gbW9sZXN0aWFlIGVycm9yIGFsaXF1YW0KIA1ldCBleGVyY2l0YXRpb25lbSBkb2xvcmVzIGFjY3VzYW11cyB2b2x1cHRhcyBvZGlvCmF1dCBkb2xvcmlidXMgbm9uIGV0IHNhZXBlIGV4cGxpY2FibyBtb2xsaXRpYSBhdXQgbmVtbwpuZW1vIGxhYm9ydW0gZG9sb3JlbXF1ZSBhY2N1c2FudGl1bQppbiBuaXNpIGVzdCBudW1xdWFtIHF1aWEKIA1sYWJvcmlvc2FtIGFyY2hpdGVjdG8gbmFtCmRvbG9yaWJ1cyBhc3BlcmlvcmVzIGlkIHF1aSB0ZW1wb3JhIGxhYm9ydW0gcXVvZCBkb2xvcmVtCnF1aSB2ZWxpdCB1dCByZXJ1bSBzZXF1aSBhbGlxdWFtCnZlbCBlc3QgbWF4aW1lIGVuaW0gcGVyc3BpY2lhdGlzIHByYWVzZW50aXVtIGV0IGVycm9y';
 
-var urlFixture = 'http://uZn:pZw@192.168.10.12:1025/me/_de/me?include_docs=true';
+var urlObj = {
+  'scheme': 'http',
+  'user': 'uZn',
+  'pass': 'pZw',
+  'host': '192.168.10.12:1025',
+  'uri': '/me/_de/me?include_docs=true'
+};
+var urlFixture = urlObj.scheme + '://' + urlObj.user + ':' + urlObj.pass + '@' + urlObj.host + urlObj.uri;
 
-describe('fetchDocs', function() {
+describe('fetchDocs()', function() {
 
   var nock_http;
 
   beforeEach(function() {
     // reset nock interceptor
-    nock_http = nock('http://192.168.10.12:1025')
-      .get('/me/_de/me?include_docs=true');
+    nock_http = nock(urlObj.scheme + '://' + urlObj.host)
+      .get(urlObj.uri);
   });
 
-  it('requests the specified URL', function() {
-    var mock_http = sinon.mock(http, 'request', function(fetchURL) {
-      return expect(fetchURL).to.equal(urlFixture);
+  it('requests the specified URL', function(done) {
+    nock_http.reply(200, function(uri) {
+      expect(uri).to.equal(urlObj.uri);
+      done();
+      return dl_data;
     });
-    cdbfuncs.fetchDocs(mock_http, urlFixture);
+    cdbfuncs.fetchDocs(http, urlFixture);
   });
 
   it('returns data via promise without modification', function() {
