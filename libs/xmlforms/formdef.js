a920b235ad25ea1e7dad1964d249ac8b7b3a37af
Bryan <btbonval@gmail.com>
2016-02-11 04:39:11 -0500

    first two functions pass unit tests, notes on third

--- libs/xmlforms/formdef.js
+++ libs/xmlforms/formdef.js
@@ -1,18 +1,28 @@
 var Promise = require('../common').Promise;
 
-exports.fetchFormDefs = function() {
-  return new Promise(function (resolve) {
-    return resolve();
-  });
+var xmleng = require('pixl-xml');
+
+exports.fetchFormDefs = function(db, pgsql) {
+  // fetches a list of objects with base64 attachment in the "form" field.
+  // unwraps the objects and decodes the base64
+  return db.query(pgsql.getFormDefinitionsXML())
+           .then(function (formlist) {
+             return formlist.map(function (el) {
+               return new Buffer(el.form, 'base64').toString('utf8');
+             });
+           }, console.log);
 };
 
-exports.filterInstanceXML = function() {
+exports.filterInstanceXML = function(xmldata) {
   return new Promise(function (resolve) {
-    return resolve();
+    var xml = xmleng.parse(xmldata, { 'preserveAttributes': true } );
+    return resolve(xmleng.stringify(xml['h:head'].model.instance,
+                                    'instance'));
   });
 };
 
 exports.parseFormDefXML = function() {
+  // DFS through xmleng.parse returning leaf with full path to leaf.
   return new Promise(function (resolve) {
     return resolve();
   });
