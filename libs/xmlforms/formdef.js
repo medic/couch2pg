1bef0254e8edda147c14e067b07a75088569a6af
Bryan <btbonval@gmail.com>
2016-02-27 22:47:00 -0500

    code passes tests
    refactored XML parser for reuse

--- libs/xmlforms/formdef.js
+++ libs/xmlforms/formdef.js
@@ -42,33 +42,46 @@ var flattenObj = function(x, result, prefix) {
   return result;
 };
 
+exports.parseInstanceXML = function(xml) {
+  // parse to json
+  var jsondata = xmleng.parse(xml, { 'preserveAttributes': true } );
+  // pick off form's name
+  var formname = xmleng.hashKeysToArray(jsondata)[0];
+  // skip contact forms (for now)
+  if (formname === 'data') {
+    return;
+  }
+  // grab subset of data
+  jsondata = jsondata[formname];
+  // flatten the leaf tags
+  var flattaglist = Object.keys(flattenObj(jsondata, {}));
+  // filter out anything containing `_Attribs`. tag attributes not needed.
+  flattaglist = flattaglist.filter(function (el) {
+    return el.indexOf('_Attribs') === -1;
+  });
+  return {
+    'formname': formname,
+    'jsondata': jsondata,
+    'fields': flattaglist
+  };
+};
+
 exports.parseFormDefXML = function(xmldatalist) {
   return new Promise(function (resolve) {
     // each form's name acts as index
     var flatdefs = {};
     // iterate over each form
     xmldatalist.forEach(function (xmldata) {
-      // parse to json
-      var jsondata = xmleng.parse(xmldata, { 'preserveAttributes': true } );
-      // pick off form's name
-      var formname = xmleng.hashKeysToArray(jsondata)[0];
-      // skip contact forms (for now)
-      if (formname === 'data') {
+      var flatxml = exports.parseInstanceXML(xmldata);
+      if (!flatxml) {
         return;
       }
-      // grab subset of data
-      jsondata = jsondata[formname];
-      // flatten the leaf tags
-      var flattaglist = Object.keys(flattenObj(jsondata, {}));
-      // filter out anything containing `_Attribs`. tag attributes not needed.
-      flattaglist = flattaglist.filter(function (el) {
-        return el.indexOf('_Attribs') === -1;
-      });
+      var formname = flatxml.formname;
       // track the leaves wrapped up in the form name
       flatdefs[formname] = {};
-      flatdefs[formname].fields = flattaglist;
+      flatdefs[formname].fields = flatxml.fields;
       // apply version attribute taken from form's eponymous tag
-      flatdefs[formname].version = jsondata._Attribs.version;
+      flatdefs[formname].version = flatxml.jsondata._Attribs.version;
     });
     return resolve(flatdefs);
   });
--- libs/xmlforms/formreport.js
+++ libs/xmlforms/formreport.js
@@ -1,27 +1,46 @@
-var Promise = require('../common').Promise;
+var parseInstanceXML = require('./formdef').parseInstanceXML;
 
-//var xmleng = require('pixl-xml');
-
-exports.fetchAndParseReports = function() {
+exports.fetchAndParseReports = function(db, pgsql) {
   // fetches a list of UUIDs of reports in the couch database.
-  return new Promise(function (resolve) {
-    return resolve('Fail!');
+  return db.query(pgsql.fetchMissingReportContents()).then(function (data) {
+    return Object.keys(data).map(function (i) {
+      // extract XML and add a root tag around it to reuse existing code
+      // which expects wrapper
+      var flatxml = parseInstanceXML('<instance>' + data[i].xml + '</instance>');
+      // build object from nested field names
+      var xmldict = {};
+      flatxml.fields.forEach(function (field) {
+        // use single strings to resolve deeply nested properties
+        // adapted from http://stackoverflow.com/a/22129960/1867779
+        xmldict[field] = field.split('/').reduce(function(prev, curr) {
+          return  prev ? prev[curr] : undefined;
+        }, flatxml.jsondata);
+      });
+      // return object with expected properties
+      return {
+        'id': data[i].uuid,
+        'formname': flatxml.formname,
+        'formversion': flatxml.jsondata._Attribs.version,
+        'chw': data[i].chw,
+        'reported': data[i].reported,
+        'xml': xmldict
+      };
+    });
   });
 };
 
-exports.storeMetaData = function() {
+exports.storeMetaData = function(db, pgsql, objects) {
   // stores metadata from a list of objects fashioned as defined in
   // pgsql.writeReportMetaData()
-  return new Promise(function (resolve) {
-    return resolve('Fail!');
+  return db.query(pgsql.writeReportMetaData(objects)).then(function () {
+    return objects;
   });
 };
 
-exports.storeReports = function() {
+exports.storeReports = function(db, pgsql, objects, formDefs) {
   // stores form field data from a list of objects fashioned as defined in
   // pgsql.writeReportMetaData() ... which also applies to
   // pgsql.writeReportContents()
-  return new Promise(function (resolve) {
-    return resolve('Fail!');
-  });
+  // requires formDefs as defined in pgsql.writeReportContents()
+  return db.query(pgsql.writeReportContents(objects, formDefs));
 };
