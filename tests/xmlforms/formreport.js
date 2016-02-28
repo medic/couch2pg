8a80c544a93d50e80f5b899d4a0b8be33228db06
Bryan <btbonval@gmail.com>
2016-02-27 20:56:19 -0500

    update old test for new expectation
    also making a test more specific

--- tests/xmlforms/formreport.js
+++ tests/xmlforms/formreport.js
@@ -51,57 +51,29 @@ var formContents = require('./fixtures/formcontents.json');
 
 var formDefinition = require('./fixtures/formdefinition.json');
 
-// represents UUIDs of documents in Couch reflection
-// list of { 'uuid': 'X' }
-var db_ids = formContents.map(function (el) {
-  return { 'uuid': el.id };
-});
+var dbContents = require('./fixtures/formcontentsdb.json');
+
 
 describe('Form Reports XML Handler', function() {
 
   describe('fetchAndParseReports()', function () {
+    var callstack = [];
+    var result = {};
+    before(function (done) {
+      var this_db = dbgen(callstack, [dbContents]);
+      formreport.fetchAndParseReports(this_db, pgsql).then(function (val) {
+        result = val;
+        return done();
+      }, done);
+    });
 
-    describe('with none already parsed', function () {
-      var callstack = [];
-      var result = {};
-      before(function (done) {
-        var this_db = dbgen(callstack, [[]]);
-        formreport.fetchAndParseReports(this_db, pgsql).then(function (val) {
-          result = val;
-          return done();
-        }, done);
-      });
+    it('passes expected query', function () {
+      return expect(callstack[0]).to.equal(pgsql.fetchMissingReportContents());
+    });
 
-      it('passes expected query', function () {
-        return expect(callstack[0]).to.equal(pgsql.fetchMissingReportContents());
-      });
-
-      it('returns all docs', function () {
-        return expect(result).to.deep.equal(db_ids);
-      });
-    }); // none parsed
-
-    describe('with two already parsed', function () {
-      var callstack = [];
-      var result = {};
-      var already_parsed = db_ids.slice(1,3); // two are parsed
-      var not_parsed = [db_ids[0]].concat(db_ids.slice(3)); // all but two left
-      before(function (done) {
-        var this_db = dbgen(callstack, [already_parsed]);
-        formreport.fetchAndParseReports(this_db, pgsql).then(function (val) {
-          result = val;
-          return done();
-        }, done);
-      });
-
-      it('passes expected query', function () {
-        return expect(callstack[0]).to.equal(pgsql.fetchMissingReportContents());
-      });
-
-      it('returns missing docs', function () {
-        return expect(result).to.deep.equal(not_parsed);
-      });
-    }); // two parsed
+    it('returns all docs', function () {
+      return expect(result).to.deep.equal(formContents);
+    });
 
   }); // fetchAndParseReports
 
@@ -141,7 +113,7 @@ describe('Form Reports XML Handler', function() {
     it('passes expected query', function() {
       // just look for the basic text. pgsql will modify the inputs, so
       // pass no inputs to generate only the text.
-      return expect(callstack[0]).to.contain(pgsql.writeReportContents(formContents, formDefinition));
+      return expect(callstack[0]).to.equal(pgsql.writeReportContents(formContents, formDefinition));
     });
 
   });
