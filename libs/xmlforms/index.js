ffe6f220bfb84cd7e2b51cac47cf4928b90b5bc2
Bryan <btbonval@gmail.com>
2016-03-02 03:01:10 -0500

    update integration to fetch form report contents

--- libs/xmlforms/index.js
+++ libs/xmlforms/index.js
@@ -4,10 +4,12 @@ var Promise = require('../common').Promise;
 var handleError = require('../common').handleError;
 
 var formdef = require('./formdef');
+var formreport = require('./formreport');
 var pgsql = require('./pgsql');
 
 module.exports = function () {
   var db;
+  var stickyFormDefs;
   return pglib({ 'promiseLib': Promise })(process.env.POSTGRESQL_URL)
     .connect()
     .then(function (this_db) {
@@ -26,12 +28,27 @@ module.exports = function () {
       return formdef.parseFormDefXML(listOfXMLStrings);
     }, handleError)
     .then(function (formDefs) {
+      stickyFormDefs = formDefs;
       console.log('write out list of ' + Object.keys(formDefs).length + ' forms to form_list table');
       return formdef.writeFormList(db, pgsql, formDefs);
     }, handleError)
     .then(function (formDefs) {
       console.log('create a table for each form as formview_{formname}');
       return formdef.writeFormViews(db, pgsql, formDefs);
+    }, handleError)
+    .then(function () {
+      console.log('find unparsed reports');
+      return formreport.fetchAndParseReports(db, pgsql);
+    }, handleError)
+    .then(function (reports) {
+      console.log('found ' + reports.length + ' reports not yet parsed.');
+      console.log('writing report metadata to database');
+      return formreport.storeMetaData(db, pgsql, reports);
+    }, handleError)
+    .then(function (reports) {
+      console.log('using list of ' + Object.keys(stickyFormDefs).length + ' forms for report handling');
+      console.log('writing report data to database');
+      return formreport.storeReports(db, pgsql, reports, stickyFormDefs);
     }, handleError)
     .catch(handleError)
     .finally(function () {
