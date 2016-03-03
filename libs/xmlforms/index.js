6fbb40ccebaa483f1c940d11cb86a451c41772c8
Bryan <btbonval@gmail.com>
2016-03-03 04:44:52 -0500

    changing integration and SQL for table definitions directly from forms

--- libs/xmlforms/index.js
+++ libs/xmlforms/index.js
@@ -3,52 +3,32 @@ var pglib = require('pg-promise');
 var Promise = require('../common').Promise;
 var handleError = require('../common').handleError;
 
-var formdef = require('./formdef');
 var formreport = require('./formreport');
 var pgsql = require('./pgsql');
 
 module.exports = function () {
   var db;
-  var stickyFormDefs;
   return pglib({ 'promiseLib': Promise })(process.env.POSTGRESQL_URL)
     .connect()
     .then(function (this_db) {
       db = this_db;
     })
     .then(function () {
-      console.log('fetching XML form definitions');
-      return formdef.fetchFormDefs(db, pgsql);
-    })
-    .then(function (listOfXMLStrings) {
-      console.log('extracting <instance> from ' + listOfXMLStrings.length + ' XML definitions');
-      return formdef.filterInstanceXML(listOfXMLStrings);
-    }, handleError)
-    .then(function (listOfXMLStrings) {
-      console.log('converting ' + listOfXMLStrings.length + ' <instance> tags into an object of forms and flat lists of fields and form versions.');
-      return formdef.parseFormDefXML(listOfXMLStrings);
-    }, handleError)
-    .then(function (formDefs) {
-      stickyFormDefs = formDefs;
-      console.log('write out list of ' + Object.keys(formDefs).length + ' forms to form_list table');
-      return formdef.writeFormList(db, pgsql, formDefs);
-    }, handleError)
-    .then(function (formDefs) {
-      console.log('create a table for each form as formview_{formname}');
-      return formdef.writeFormViews(db, pgsql, formDefs);
-    }, handleError)
-    .then(function () {
-      console.log('find unparsed reports');
+      console.log('create metadata table');
+      console.log('find unparsed reports and parse them');
       return formreport.fetchAndParseReports(db, pgsql);
     }, handleError)
     .then(function (reports) {
-      console.log('found ' + reports.length + ' reports not yet parsed.');
+      console.log('create form tables to store reports');
+      return formreport.createTables(db, pgsql, reports);
+    }, handleError)
+    .then(function (reports) {
       console.log('writing report metadata to database');
       return formreport.storeMetaData(db, pgsql, reports);
     }, handleError)
     .then(function (reports) {
-      console.log('using list of ' + Object.keys(stickyFormDefs).length + ' forms for report handling');
       console.log('writing report data to database');
-      return formreport.storeReports(db, pgsql, reports, stickyFormDefs);
+      return formreport.storeReports(db, pgsql, reports);
     }, handleError)
     .catch(handleError)
     .finally(function () {
--- libs/xmlforms/pgsql.js
+++ libs/xmlforms/pgsql.js
@@ -21,27 +21,6 @@ exports.putFormList = function(formlist) {
   return 'DROP TABLE IF EXISTS form_list; CREATE TABLE form_list (name TEXT); INSERT INTO form_list VALUES ' + formlist;
 };
 
-exports.putFormViews = function(formdef) {
-  // expects an obj of {
-  //   'form1': {
-  //     'fields': ['field1','field2',...],
-  //     'version': 'x'
-  //  },
-  //  'form2':
-  //    ...
-  var manyQueries = '';
-  Object.keys(formdef).forEach(function (formName) {
-    var thisForm = formdef[formName];
-    manyQueries += scrub('CREATE TABLE IF NOT EXISTS %I (',
-                         'formview_' + formName + '_' + formdef[formName].version);
-    var fields = thisForm.fields.map(function (attr) {
-      return scrub('%I', attr) + ' TEXT';
-    }).join(',');
-    manyQueries += fields + ');';
-  });
-  return manyQueries;
-};
-
 exports.fetchMissingReportContents = function() {
   var c = getFromEnv();
   // reduce database calls by appending a CREATE with new return value.
@@ -49,6 +28,25 @@ exports.fetchMissingReportContents = function() {
   // grab specific report fields for each report in Couch but not in formmeta
   var get_contents = scrub('SELECT %I->>\'_id\' AS uuid, %I->>\'reported_date\' AS reported, %I#>\'{contact,parent,_id}\' AS chw, %I->>\'content\' AS xml FROM %I LEFT OUTER JOIN form_metadata AS fmd ON (fmd.uuid = %I->>\'_id\') WHERE %I @> \'{"type": "data_record"}\'::jsonb AND %I ? \'form\' AND fmd.uuid IS NULL;', c.jsonCol, c.jsonCol, c.jsonCol, c.jsonCol, c.jsonTable, c.jsonCol, c.jsonCol, c.jsonCol);
   return create + get_contents;
+};
+
+exports.putFormViews = function(tabledef) {
+  // expects an obj of {
+  //   'formview_formname_formversion': ['field1','field2',...],
+  //   ...
+  //  }
+  var manyQueries = '';
+  Object.keys(tabledef).forEach(function (tableName) {
+    // map the fields to include a type
+    manyQueries += scrub('CREATE TABLE IF NOT EXISTS %I (', tableName);
+    // scrub field names and append TEXT type to each
+    var fields = tabledef[tableName].map(function (attr) {
+      return scrub('%I', attr) + ' TEXT';
+    }).join(',');
+    // finalize CREATE TABLE
+    manyQueries += fields + ');';
+  });
+  return manyQueries;
 };
 
 exports.writeReportMetaData = function (objs) {
@@ -73,56 +71,26 @@ exports.writeReportMetaData = function (objs) {
   return scrub('INSERT INTO form_metadata (uuid, chw, formname, formversion, reported) VALUES %L;', values);
 };
 
-var tableToForm = function(table) {
-  // spearate formview_ off.
-  // separate _year[ hh:mm:ss] off the end
-  return table.split('formview_')[1].split('_').slice(0,-1).join('_');
-};
-
-exports.writeReportContents = function(objs, tabledefs) {
-  // expect same objs as writeFormMetaData
-  // tabledefs should be: {
-  //   'tablename': {
-  //     'fields': ['col1','col2','col3'...],
-  //     'version': 'date'
-  //   }, ...
-  // }
-  // this format is produced by formdef.parseFormDefXML()
-
-  // sort objects into separate tables
-  var dataset = {};
-  objs.forEach(function (el) {
-    var table = 'formview_' + el.formname + '_' + el.formversion;
-    // create list for table if one doesn't exist
-    if (dataset[table] === undefined) {
-      dataset[table] = [];
-    }
-    // store elements by their table
-    dataset[table].push(el);
-  });
-
-  // perform inserts grouped as one per table.
-  return Object.keys(dataset).filter(function (table) {
-    // skip any tables which are not found in the table definitions
-    if (tabledefs[tableToForm(table)] === undefined) {
-      return false;
-    } else {
-      return true;
-    }
-  }).map(function (table) {
-    // reduce table name back to base form name
-    var form = tableToForm(table);
-    // order element fields of objects for this table using tabledef
-    var orderedDataset = dataset[table].map(function (el) {
-      // use tabledef field order
-      // only one version for any form definition can be found for now.
-      return tabledefs[form].fields.map(function (field) {
-        // return element's values for tabledef's field
-        return el.xml[field];
+exports.writeReportContents = function(objs) {
+  // take in list of { 'tablename':
+  //   'fields': [...],
+  //   'docs': [...]
+  // }, ... }
+  // and write each contained doc to the proper table
+  var inserts = '';
+  // prepare one INSERT for each table
+  Object.keys(objs).forEach(function (tableName) {
+    var table = objs[tableName];
+    var fields = table.fields;
+    // compress all values into a list of lists in the same order as fields.
+    var values = table.docs.map(function (doc) {
+      // order by fields
+      return fields.map(function (field) {
+        // extract each field's value from doc
+        return doc.xml[field];
       });
     });
-
-    // prepare insert definition
-    return scrub('INSERT INTO %I (%I) VALUES %L', table, tabledefs[form].fields, orderedDataset);
-  }).join('; ');
+    inserts += scrub('INSERT INTO %I (%I) VALUES %L;', tableName, fields, values);
+  });
+  return inserts;
 };
