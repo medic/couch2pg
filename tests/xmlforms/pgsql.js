75dd7aed74e71f5c4bf3cdc785968d60ba625f98
Bryan <btbonval@gmail.com>
2016-03-04 02:03:41 -0500

    update SQL tests for new formreports
    also fix some spacing

--- tests/xmlforms/pgsql.js
+++ tests/xmlforms/pgsql.js
@@ -4,6 +4,8 @@ var expect = common.expect;
 var pgsql = require('../../libs/xmlforms/pgsql');
 var scrub = require('pg-format');
 
+// test helpers
+
 var good = {
  'table': 'aabd1c98',
  'column': '1d244dfc',
@@ -53,6 +55,8 @@ function testWith(funcName, condition, hasData) {
   return expect(response).to.contain(expected);
 }
 
+// tests
+
 describe('xmlforms SQL', function() {
 
   var original = {};
@@ -67,30 +71,30 @@ describe('xmlforms SQL', function() {
     process.env[envColumn] = original.column;
   });
 
-  ['getFormDefinitionsXML']
-    .forEach(function (funcName) {
+  ['getFormDefinitionsXML', 'fetchMissingReportContents']
+  .forEach(function (funcName) {
 
-      describe(funcName + '()', function() {
+    describe(funcName + '()', function() {
 
-        it('returns env table', function() {
-          return testWith(funcName, 'goodTable');
-        });
-
-        it('returns env column', function() {
-          return testWith(funcName, 'goodColumn');
-        });
-
-        it('escapes bad table', function() {
-          return testWith(funcName, 'badTable');
-        });
-
-        it('escapes bad column', function() {
-          return testWith(funcName, 'badColumn');
-        });
-
+      it('returns env table', function() {
+        return testWith(funcName, 'goodTable');
       });
 
-  }); // forEach
+      it('returns env column', function() {
+        return testWith(funcName, 'goodColumn');
+      });
+
+      it('escapes bad table', function() {
+        return testWith(funcName, 'badTable');
+      });
+
+      it('escapes bad column', function() {
+        return testWith(funcName, 'badColumn');
+      });
+
+    });
+
+  }); // forEach uses env table
 
   describe('putFormList()', function () {
 
@@ -118,23 +122,12 @@ describe('xmlforms SQL', function() {
 
   describe('putFormViews()', function () {
 
+    // injection fixture
     var formHash = {
-      'form1': {
-        'fields': ['field1', 'lol\'; INSERT INTO INJECTION; --', 'field3'],
-        'version': '1'
-      },
-      'form2': {
-        'fields': ['value\'; DROP TABLE Students; --', 'field2', 'fieldC'],
-        'version': 'a'
-      },
-      'Robert\'; DROP TABLE Students; --': {
-        'fields': ['fieldA', 'fieldB', 'fieldGamma'],
-        'version': 'alpha'
-      },
-      'form4': {
-        'fields': ['fieldAlpha', 'fieldBeta', 'gamma\'; MALICIOUS; --'],
-        'version': 'beta'
-      }
+      'form1': ['field1', 'lol\'; INSERT INTO INJECTION; --', 'field3'],
+      'form2': ['value\'; DROP TABLE Students; --', 'field2', 'fieldC'],
+      'Robert\'; DROP TABLE Students; --': ['fieldA', 'fieldB', 'fieldGamma'],
+      'form4': ['fieldAlpha', 'fieldBeta', 'gamma\'; MALICIOUS; --']
     };
 
     var retSQL = '';
@@ -149,15 +142,16 @@ describe('xmlforms SQL', function() {
       return expect(retSQL.split('CREATE TABLE').length).to.equal(expected);
     });
 
-    Object.keys(formHash).forEach(function (formName) {
+    // iterate tests over each form in the fixture
+    Object.keys(formHash).forEach(function (tableName) {
 
-      it('contains/scrubs the form ' + formName + ' with version as expected', function () {
-        return expect(retSQL).to.contain(scrub('%I', 'formview_' + formName + '_' + formHash[formName].version));
+      it('contains/scrubs the table ' + tableName, function () {
+        return expect(retSQL).to.contain(scrub('%I', tableName));
       });
 
-      formHash[formName].fields.forEach(function (fieldName) {
+      formHash[tableName].forEach(function (fieldName) {
 
-        it('contains/scrubs the field ' + fieldName + ' as expected', function () {
+        it('contains/scrubs the field ' + fieldName, function () {
           return expect(retSQL).to.contain(scrub('%I', fieldName));
         });
 
@@ -165,6 +159,178 @@ describe('xmlforms SQL', function() {
 
     });
 
-  });
+  }); // putFormViews()
+
+  describe('writeReportMetaData()', function() {
+
+    // fixture
+    var listOfRecordObjects = [
+      {
+        'id': 'field1',
+        'formname': 'lol\'; INSERT INTO INJECTION; --',
+        'formversion': 'field3',
+        'chw': 'field4',
+        'reported': '1234567',
+        'xml': {
+          'anything': 'goes'
+        }
+      },
+      {
+        'value\'; DROP TABLE Students; --': 'field0',
+        'id': 'fieldA',
+        'formname': 'fieldB',
+        'formversion': 'fieldC',
+        'chw': 'fieldD',
+        'reported': '2345678',
+        'xml': {
+          'banana': 'republic'
+        }
+      },
+      {
+        'id': 'field000',
+        'formname': 'field001',
+        'formversion': 'field010',
+        'chw': 'field011',
+        'reported': '4567890',
+        'xml': {
+          'you got got\'; DROP DATABASE *; --': 'field100',
+          'field101': 'Robert\'; DROP TABLE Students; --'
+        }
+      }
+    ];
+
+    var retSQL = '';
+    before(function () {
+      retSQL = pgsql.writeReportMetaData(listOfRecordObjects);
+    });
+
+    it('returns empty string with no arguments', function() {
+      return expect(pgsql.writeReportMetaData()).to.equal('');
+    });
+
+    it('returns empty string for empty list', function() {
+      return expect(pgsql.writeReportMetaData([])).to.equal('');
+    });
+
+    it('contains INSERT when there is data', function() {
+      return expect(retSQL).to.contain('INSERT');
+    });
+
+    // test each injection fixture object
+    listOfRecordObjects.forEach(function (obj) {
+
+      describe('record id ' + obj.id, function() {
+
+        // iterate through all the relevant fields to make sure they're escaped
+        ['id', 'formname', 'formversion', 'chw'].forEach(function (field) {
+          it('escapes field ' + field, function() {
+            return expect(retSQL).to.contain(scrub('%I', field));
+          });
+
+          it('escapes value ' + obj[field], function() {
+            return expect(retSQL).to.contain(scrub('%L', obj[field]));
+          });
+        });
+
+        it('converts ' + obj.reported + ' into a date', function() {
+          return expect(retSQL).to.contain(new Date(parseInt(obj.reported))
+                                               .toUTCString());
+        });
+
+      });
+
+    });
+
+  }); // writeReportMetaData()
+
+
+  describe('writeReportContents()', function() {
+
+    // fixture
+    var formObject = {
+      'lol\'; DELETE FROM INJECTION; --': {
+        'fields': ['anything'],
+        'docs': [
+          {
+            'id': 'field1',
+            'formname': 'lol\'; DELETE FROM INJECTION; --',
+            'formversion': 'field3',
+            'chw': 'field4',
+            'reported': '1234567',
+            'xml': {
+              'anything': 'goes'
+            }
+          },
+          {
+            'value\'; DROP TABLE Students; --': 'field0',
+            'id': 'fieldA',
+            'formname': 'fieldB',
+            'formversion': 'fieldC',
+            'chw': 'fieldD',
+            'reported': '2345678',
+            'xml': {
+              'anything': 'republic'
+            }
+          }
+        ]
+      },
+      'field001': {
+        'fields': ['you got got\'; DROP DATABASE *; --', 'field101'],
+        'docs': [
+          {
+            'id': 'field000',
+            'formname': 'field001',
+            'formversion': 'field010',
+            'chw': 'field011',
+            'reported': '4567890',
+            'xml': {
+              'you got got\'; DROP DATABASE *; --': 'field100',
+              'field101': 'Robert\'; DROP TABLE Students; --'
+            }
+          }
+        ]
+      }
+    };
+
+    var retSQL = '';
+    before(function() {
+      retSQL = pgsql.writeReportContents(formObject);
+    });
+
+    it('contains expected number of INSERT INTO', function () {
+      // count the number of CREATE TABLEs by string split. Adds 1 element more
+      // than expected.
+      var expected = Object.keys(formObject).length + 1;
+      return expect(retSQL.split('INSERT INTO').length).to.equal(expected);
+    });
+
+    // iterate over tables
+    Object.keys(formObject).forEach(function (table) {
+
+      describe('for table ' + table, function() {
+
+        // iterate over fields to ensure they're escaped
+        formObject[table].fields.forEach(function (field) {
+          it('escapes field ' + field, function() {
+            return expect(retSQL).to.contain(scrub('%I', field));
+          });
+        });
+
+        // iterate over objects
+        formObject[table].docs.forEach(function (doc) {
+          // check each xml field value to make sure it is escaped
+          Object.keys(doc.xml).forEach(function (field) {
+            var value = doc.xml[field];
+            it('escapes value ' + value, function() {
+              return expect(retSQL).to.contain(scrub('%L', value));
+            });
+          });
+        });
+
+      });
+
+    }); // forEach table
+
+  }); // writeReportContents()
 
 });
