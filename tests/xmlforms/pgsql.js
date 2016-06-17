df20a46876256076b9a8ad72e406f2f1835bdf72
Stefan du Fresne <stefan@sdufresne.info>
2016-06-17 14:29:46 +0100

    Fixing whitespace medic/medic-webapp#2124

--- tests/xmlforms/pgsql.js
+++ tests/xmlforms/pgsql.js
@@ -29,7 +29,7 @@ describe('xmlforms SQL', function() {
   describe('initializeFormMetadata', function () {
 
     it('returns a very specific SQL string', function () {
-      expect(pgsql.initializeFormMetadata()).to.equal('CREATE TABLE form_metadata (uuid TEXT, chw TEXT, chw_area TEXT, formname TEXT, formversion TEXT, reported TIMESTAMP); CREATE INDEX form_metadata_uuid ON form_metadata (uuid); CREATE INDEX form_metadata_chw ON form_metadata (chw); CREATE INDEX form_metadata_reported ON form_metadata (reported); CREATE INDEX form_metadata_formname ON form_metadata (formname); CREATE INDEX form_metadata_formversion ON form_metadata (formname, formversion);');
+      expect(pgsql.initializeFormMetadata()).to.equal('CREATE TABLE form_metadata (uuid TEXT, chw TEXT, chw_area TEXT, formname TEXT, formversion TEXT, reported TIMESTAMP); CREATE INDEX form_metadata_uuid ON form_metadata (uuid); CREATE INDEX form_metadata_chw ON form_metadata (chw); CREATE INDEX form_metadata_reported ON form_metadata (reported); CREATE INDEX form_metadata_formname ON form_metadata (formname); CREATE INDEX form_metadata_formversion ON form_metadata (formname, formversion); ');
     });
 
   });
