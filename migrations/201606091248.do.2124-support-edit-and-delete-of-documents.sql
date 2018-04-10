f91e1b83cdf3c649f07b9d119e142d8897c4fa1a
Victor Gomez <victor@medicmobile.org>
2018-04-10 14:28:45 -0600

    Updating existing migration

--- migrations/201606091248.do.2124-support-edit-and-delete-of-documents.sql
+++ migrations/201606091248.do.2124-support-edit-and-delete-of-documents.sql
@@ -1,3 +1,2 @@
--- CREATE TABLE IF NOT EXISTS couchdb_progress (seq int);
-CREATE TABLE couchdb_progress (seq int);
+CREATE TABLE IF NOT EXISTS couchdb_progress (seq int);
 INSERT INTO couchdb_progress VALUES (0);
